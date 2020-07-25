c29ff2ba5aa5ccc0897abda51de5e924a29c0e14
EddieMa <machaoxue@yeah.net>
2020-07-25 19:06:20 +0800

    eventHub 实现数据通信

--- src/index.js
+++ src/index.js
@@ -1,15 +1,145 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+
+let fnLists = {}
+const eventHub = {
+  on(eventName, fn) {
+    if (!fnLists[eventName]) { fnLists[eventName] = [] }
+    fnLists[eventName].push(fn)
+  },
+  trigger(eventName, data) {
+    const fnList = fnLists[eventName]
+    if (!fnList) { return }
+    fnList.forEach((fn) => fn(data))
+  }
+}
+let restTime = 100;
+
+const handleData = {
+  init() {
+    eventHub.on('play', (time) => {
+      restTime -= time
+      ReactDOM.render(
+        <App />,
+        document.getElementById('root')
+      );
+    })
+  }
+}
+
+handleData.init()
+
+function App() {
+  const state = { restTime: restTime }
+  return (
+    <div className='app'>
+      <p className='describe'>
+        有一户人家，他们有一张游乐园卡，这张卡限定总共可以玩 100 个小时，家中有四个孩子，他们可以任意使用这张卡。
+        <br />
+        <br />
+        我们现在用 eventHub 来实现一个孩子去游玩后，每个孩子都能得到剩余的游玩时间。
+      </p>
+      <main>
+        <Father1 data={state.restTime} />
+        <Father2 data={state.restTime} />
+      </main>
+    </div>
+  )
+}
+
+function Father1(props) {
+  return (
+    <div className='father1'>
+      <Son1 data={props.data} />
+      <Son2 data={props.data} />
+    </div>
+  )
+}
+
+function Father2(props) {
+  return (
+    <div className='father2'>
+      <Son3 data={props.data} />
+      <Son4 data={props.data} />
+    </div>
+  )
+}
+
+function Son1(props) {
+  const reduceTime = (time) => {
+    eventHub.trigger('play', time)
+  }
+  return (
+    <div className='son1'>
+      <div>我是Son1</div>
+      <div>剩余时间：{props.data} h</div>
+      <button onClick={reduceTime.bind(null, 1)}>玩了 1 个小时</button>
+      <button onClick={reduceTime.bind(null, 2)}>玩了 2 个小时</button>
+      <button onClick={reduceTime.bind(null, 3)}>玩了 3 个小时</button>
+    </div>
+  )
+}
+
+function Son2(props) {
+  const reduceTime = (time) => {
+    eventHub.trigger('play', time)
+  }
+  return (
+    <div className='son2'>
+      <div>我是Son2</div>
+      <div>剩余时间：{props.data} h</div>
+      <button onClick={reduceTime.bind(null, 1)}>玩了 1 个小时</button>
+      <button onClick={reduceTime.bind(null, 2)}>玩了 2 个小时</button>
+      <button onClick={reduceTime.bind(null, 3)}>玩了 3 个小时</button>
+    </div>
+  )
+}
+
+function Son3(props) {
+  const reduceTime = (time) => {
+    eventHub.trigger('play', time)
+  }
+  return (
+    <div className='son3'>
+      <div>我是Son3</div>
+      <div>剩余时间：{props.data} h</div>
+      <button onClick={reduceTime.bind(null, 1)}>玩了 1 个小时</button>
+      <button onClick={reduceTime.bind(null, 2)}>玩了 2 个小时</button>
+      <button onClick={reduceTime.bind(null, 3)}>玩了 3 个小时</button>
+    </div>
+  )
+}
+
+function Son4(props) {
+  const reduceTime = (time) => {
+    eventHub.trigger('play', time)
+  }
+  return (
+    <div className='son4'>
+      <div>我是Son4</div>
+      <div>剩余时间：{props.data} h</div>
+      <button onClick={reduceTime.bind(null, 1)}>玩了 1 个小时</button>
+      <button onClick={reduceTime.bind(null, 2)}>玩了 2 个小时</button>
+      <button onClick={reduceTime.bind(null, 3)}>玩了 3 个小时</button>
+    </div>
+  )
+}
+
+function myRender() {
+
+  console.log('render执行了')
+}
+myRender()
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
